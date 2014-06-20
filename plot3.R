5179b248ec7141bf82d88c5d832135b85a5621dc
OlesyaZ <OlesyaZ@users.noreply.github.com>
2014-06-19 21:35:45 -0400

    first commit

---
+++ plot3.R
@@ -0,0 +1,17 @@
+setwd("~/Documents/Olesya/Exploratory Data Analys /National-Emissions-Inventory")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+BALT <-NEI[(NEI$fips=="24510"),]
+
+## Cross tabs  
+xt<-xtabs(Emissions~type+year,data=BALT)
+xt<-data.frame(xt)
+
+png(filename = "plot3.png", width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")         
+library(ggplot2)                  
+
+g+geom_point()+facet_grid(.~type)+geom_smooth(method="lm",aes(group = 1))
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,24 @@
+setwd("~/Documents/Olesya/Exploratory Data Analys /National-Emissions-Inventory")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+## In Source_Classification_Code.rds choose SCC with EI.Sector which contain "Coal"
+coal<-grep("Comb.*Coal|Coal.*Comb", SCC$EI.Sector, ignore.case = FALSE)
+SCC_num<-SCC$SCC[coal]
+SCC_num<-as.character(SCC_num)
+SCC_num<-as.numeric(SCC_num)
+
+## Subset data 
+
+Combation <-NEI[NEI$SCC %in% SCC_num,]
+total_PM <- sapply(split(Combation$Emissions,Combation$year),sum)
+year <- unique(NEI$year)
+
+png(filename = "plot4.png", width = 480, height = 480, units = "px", pointsize = 12,
+    bg = "white")                           
+
+plot(year,total_PM,xlim=c(1998,2008),col="red",cex=0.7,pch=19)
+model<-lm(total_PM~year)
+abline(model,lwd=2)
+
+dev.off()
